fbd0a776c57381db4ff55374bf6aeaba29c4bdff
DischleyLaw <attorneys@dischleylaw.com>
2025-04-21 20:01:56 -0400

    Deploying updated intake and case result forms

--- main.py
+++ main.py
@@ -425,8 +425,8 @@ def case_result_success():
     return render_template("case_results_success.html")
 
 def init_db():
-with app.app_context():
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 if __name__ == "__main__":
     init_db()
